562bc439913ba6c7e63503b58f406d131d25b889
Sagar <autesagar56@gmail.com>
2026-05-20 21:02:48 +0530

    aboutPageUpdate

--- controller/about.js
+++ controller/about.js
@@ -1,10 +1,5 @@
 import courseHandler from "../model/course.js";
 
-export async function about(req,res){
-    if(req.session.user){
-        return res.redirect("/home");
-    }
-    else{        
-        return res.render("about",{userData:""});
-    }
+export async function about(req,res){        
+    return res.render("about",{userData:""});
 }
